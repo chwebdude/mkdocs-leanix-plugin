2a2695286eaaef30f20d1516fa8ee02ed28069cf
Fabrice Andréïs <7746357+chwebdude@users.noreply.github.com>
2021-12-27 16:23:46 +0000

    Initial

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mkdocs-leanix-plugin',
+    version='0.1.0',
+    description='A MkDocs plugin to import LeanIX data',
+    long_description='Include LeanIX Factsehhets and other stuff',
+    keywords='mkdocs leanix',
+    url='',
+    author='Fabrice Andreis',
+    author_email='webdude@duck.com',
+    license='MIT',
+    python_requires='>=2.7',
+    install_requires=[
+        'mkdocs>=1.0.4'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'leanix-plugin = mkdocs_leanix_plugin.plugin:LeanIXPlugin'
+        ]
+    }
+)
