c3d0e37be73397439a687d98b656415242f09e63
Fabrice Andréïs <7746357+chwebdude@users.noreply.github.com>
2021-12-28 17:35:27 +0000

    Generate site without rebuild

--- leanix/plugin.py
+++ leanix/plugin.py
@@ -1,5 +1,10 @@
 import os
 import sys
+import re
+import json 
+import requests 
+import logging
+
 from timeit import default_timer as timer
 from datetime import datetime, timedelta
 
@@ -7,61 +12,99 @@ from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
 
-class LeanIXPlugin(BasePlugin):
 
+log = logging.getLogger(__name__)
+
+
+class LeanIXPlugin(BasePlugin):
+    
     config_scheme = (
-        ('param', config_options.Type(str, default='')),
+        ('api_token', config_options.Type(str, default='')),
+        ('baseurl', config_options.Type(str, default='')),
+        ('workspaceid', config_options.Type(str, default='')),
     )
 
     def __init__(self):
         self.enabled = True
         self.total_time = 0
 
-    def on_serve(self, server):
-        return server
+    # def on_serve(self, server, config, builder):
+    #     return server
 
-    def on_pre_build(self, config):
-        return
+    # def on_pre_build(self, config):
+    #     return
 
-    def on_files(self, files, config):
-        return files
+    # def on_files(self, files, config):
+    #     return files
 
-    def on_nav(self, nav, config, files):
-        return nav
+    # def on_nav(self, nav, config, files):
+    #     return nav
 
-    def on_env(self, env, config, files):
-        return env
+    # def on_env(self, env, config, files):
+    #     return env
     
-    def on_config(self, config):
-        return config
+    def on_config(self, config):        
+        try:
+            auth_url = self.config['baseurl'] + '/services/mtm/v1/oauth2/token' # or something else if you have a dedicated MTM instance - you will know it if that is the case and if you don't just use this one.
+            request_url = self.config['baseurl'] + '/services/pathfinder/v1/graphql' # same thing as with the auth_url
 
-    def on_post_build(self, config):
-        return
 
-    def on_pre_template(self, template, template_name, config):
-        return template
+            response = requests.post(auth_url,
+                                    auth=('apitoken', self.config['api_token'] ),
+                                    data={'grant_type': 'client_credentials'})
+            response.raise_for_status() # this merely throws an error, if Webserver does not respond with a '200 OK'
+            access_token = response.json()['access_token']
 
-    def on_template_context(self, context, template_name, config):
-        return context
+            auth_header = 'Bearer ' + access_token
+            self.header = {'Authorization': auth_header}
+            log.debug("Authenticated against LeanIX")
+            return config
+        except:            
+            log.exception("Failed to authenticate against LeanIX - Verify that baseURL and token are correct\n\n")                    
+            raise
+        
+    # def on_post_build(self, config):
+    #     return
+
+    # def on_pre_template(self, template, template_name, config):
+    #     return template
+
+    # def on_template_context(self, context, template_name, config):
+    #     return context
     
-    def on_post_template(self, output_content, template_name, config):
-        return output_content
+    # def on_post_template(self, output_content, template_name, config):
+    #     return output_content
     
-    def on_pre_page(self, page, config, files):
-        return page
+    # def on_pre_page(self, page, config, files):
+    #     return page
 
-    def on_page_read_source(self, page, config):
-        return ""
+    # def on_page_read_source(self, page, config):
+    #     return ""
+
+    def _factsheet(self, matchobj):
+        
+        url = self.config['baseurl'] + "/services/pathfinder/v1/factSheets/d3bdeca8-8f79-4ee9-af4b-e390accf9f3d"
+
+        response = requests.get(url=url, headers=self.header)
+        response.raise_for_status()
+        
+        factsheet = response.json()['data']
+        displayName = factsheet['displayName']
+
+        
+        
+        return "!!! Factsheet summary\n\t"+displayName+"\n\tResponsible: Daniel hass"
 
     def on_page_markdown(self, markdown, page, config, files):
-        return markdown
+        pattern = r'(```leanix-factsheet)\s*\n(.*)\n(```)'
+        return re.sub(pattern, self._factsheet, markdown)
+        # return markdown
 
-    def on_page_content(self, html, page, config, files):
-        return html
+    # def on_page_content(self, html, page, config, files):
+    #     return html
 
-    def on_page_context(self, context, page, config, nav):
-        return context
+    # def on_page_context(self, context, page, config, nav):
+    #     return context
 
-    def on_post_page(self, output_content, page, config):
-        return output_content
-
+    # def on_post_page(self, output_content, page, config):
+    #     return output_content
