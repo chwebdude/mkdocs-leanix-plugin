d19bd0afcf4c196230414b5d3a32858f53f0e3bf
chwebdude <7746357+chwebdude@users.noreply.github.com>
2022-01-04 16:45:48 +0000

    Do not require a specific version for PyJWT

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'mkdocs>=1.0.4',
         'requests>=2.0.0',
-        'pyjwt>=2.0.0'
+        'pyjwt'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
